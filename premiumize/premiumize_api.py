2d7efe0f310148161a84ffabec065087c536ff52
Threak <wisp24+git@gmail.com>
2016-05-01 22:20:39 +0200

    Watch folder for torrents and premiumize for finished DLs

---
+++ premiumize/premiumize_api.py
@@ -0,0 +1,124 @@
+import requests
+import json
+
+class PremiumizeApi:
+    def __init__(self, customer_id, pin):
+        self.customer_id = customer_id
+        self.pin = pin
+
+    def print_usage(self):
+        print('hello')
+
+    def current_transfers(self):
+        api_path = '/api/transfer/list'
+        response = self.post_to_api(api_path)
+        return response
+
+    def root_folder_list(self):
+        api_path = '/api/folder/list'
+        response = self.post_to_api(api_path)
+        return response
+
+    def get_transfer_status_for_hash(self, transfer_hash):
+        transfers = self.current_transfers()
+        if transfers['status'] == 'success':
+            for transfer in transfers['transfers']:
+                if transfer['hash'] == transfer_hash:
+                    return transfer['status']
+            return 'not found'
+
+    def delete_finished_torrent_by_id(self, download_id):
+        download_hash = self.get_hash_for_id(download_id)
+        self.delete_finished_torrent_by_hash(download_hash)
+
+    def delete_finished_torrent_by_hash(self, download_hash):
+        if self.get_transfer_status_for_hash(download_hash) == 'finished':
+            return self.delete_torrent_by_hash(download_hash)
+        else:
+            error = {
+                'status': 'error',
+                'message': 'Download is not yet finished'
+            }
+            return error
+
+    def delete_torrent_by_id(self, download_id):
+        api_path = '/api/transfer/delete?type=torrent&id=' + download_id
+        response = self.post_to_api(api_path)
+        return response
+
+    def delete_torrent_by_hash(self, download_hash):
+        api_path = '/api/transfer/delete?type=torrent&id=' + download_hash
+        response = self.post_to_api(api_path)
+        return response
+
+    def get_hash_for_id(self, download_id):
+        folder_list = self.root_folder_list()
+        if folder_list['status'] == 'success':
+            for download in folder_list['content']:
+                if download['id'] == download_id:
+                    return download['hash']
+
+    def list_folders(self):
+        api_path = '/api/folder/list'
+        response = self.post_to_api(api_path)
+        return response
+
+    def get_folder_name_for_torrent_by_hash(self, download_hash):
+        folder_list = self.list_folders()
+        if folder_list['status'] == 'success':
+            for download in folder_list['content']:
+                if download.get('hash') == download_hash:
+                    return download['name']
+
+    def list_folder(self, what):
+        uploaded_ids = set()
+        folder_response = self.list_folders()
+        if folder_response['status'] == 'success':
+            folders = folder_response['content']
+            for i in (folder.get(what) for folder in folders):
+                if i:
+                    uploaded_ids.add(i)
+        return uploaded_ids
+
+    def download_finished_torrent_by_id(self, download_id):
+        download_hash = self.get_hash_for_id(download_id)
+        self.download_finished_torrent_by_hash(download_hash)
+
+    def download_finished_torrent_by_hash(self, download_hash):
+        pass
+
+    def list_urls_for_torrent_by_id(self, download_id):
+        download_hash = self.get_hash_for_id(download_id)
+        self.list_urls_for_torrent_by_hash(download_hash)
+
+    def list_urls_for_torrent_by_hash(self, download_hash):
+        torrent = self.browse_torrent_by_hash(download_hash)
+        urls = set()
+        if torrent['status'] == 'success':
+            for folder_key in torrent['content'].keys():
+                content = torrent['content'][folder_key]
+                for key in content['children'].keys():
+                    urls.add(content['children'][key].get('url'))
+        return urls
+
+    def browse_torrent_by_hash(self, download_hash):
+        api_path = '/api/torrent/browse?hash=' + download_hash
+        response = self.post_to_api(api_path)
+        return response
+
+    def upload_torrent_file(self, file_path):
+        api_path = '/api/transfer/create?type=torrent&src=SRC'
+        files = {
+            'src': open(file_path, 'rb')
+        }
+        response = self.post_to_api(api_path, files)
+        return response
+
+    def post_to_api(self, path, files={}):
+        host = 'https://www.premiumize.me'
+        params = {
+            'customer_id': self.customer_id,
+            'pin': self.pin
+        }
+        request = requests.post(host + path, params=params, files=files)
+        return json.loads(request.text)
---
+++ watch_torrent.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+from premiumize import premiumize_api as api
+from os import walk, remove, makedirs
+from os.path import join, exists
+from json import dumps
+from subprocess import call
+import configparser
+import threading
+import time
+
+CONFIG_PATH = 'config.ini'
+config = configparser.ConfigParser()
+config.read(CONFIG_PATH)
+
+UPLOADED_IDS = set()
+
+premiumize_api = api.PremiumizeApi(config['PREMIUMIZE']['CustomerId'], config['PREMIUMIZE']['Pin'])
+
+def print_json(json):
+    print(dumps(json, sort_keys=True, indent=4))
+
+def save_new_id(old_ids):
+    all_ids = premiumize_api.list_folder('hash')
+    new_ids = all_ids - old_ids
+    if new_ids:
+        new_id = new_ids.pop()
+        config.read(CONFIG_PATH)
+        config['DOWNLOADS']['Hashes'] = config['DOWNLOADS']['Hashes'] + ',' + new_id
+        save_config_file()
+
+def upload_torrent_from_folder():
+    torrent_folder = config['TORRENT']['TorrentFilesLocation']
+    while True:
+        for (dirpath, dirnames, filenames) in walk(torrent_folder):
+            for filename in filenames:
+                if filename.endswith('.torrent'):
+                    filepath = join(dirpath, filename)
+                    print('Now uploading: ', filename)
+                    old_folder_ids = premiumize_api.list_folder('hash')
+                    upload = premiumize_api.upload_torrent_file(filepath)
+                    if upload['status'] == 'success':
+                        print('Successfully uploaded: ' + filename)
+                        save_new_id(old_folder_ids)
+                        if config['TORRENT'].getboolean('DeleteTorrentOnSuccess', fallback=True):
+                            remove(filepath)
+                    else:
+                        print('Error:', upload['message'], 'in file', filename)
+                        if upload['message'] == 'This torrent is already in the download list.':
+                            save_new_id(old_folder_ids)
+                            if config['TORRENT'].getboolean('DeleteTorrentOnDuplicate', fallback=True):
+                                print('Deleting already existing torrent: ' + filename)
+                                remove(filepath)
+                print()
+        time.sleep(10)
+
+def download_finished_torrents():
+    download_folder = config['TORRENT']['DownloadDirectory']
+    while True:
+        config.read(CONFIG_PATH)
+        download_hash_list = config['DOWNLOADS']['Hashes'].split(',')
+        finished_hash_list = set()
+        for download_hash in download_hash_list:
+            folder_name = premiumize_api.get_folder_name_for_torrent_by_hash(download_hash)
+            if not folder_name:
+                continue
+            download_target = join(download_folder, folder_name)
+            if not exists(download_target):
+                makedirs(download_target)
+            downloads = premiumize_api.list_urls_for_torrent_by_hash(download_hash)
+            for download in downloads:
+                call(['aria2c', '-c', '-d', download_target, download])
+            if config['TORRENT'].getboolean('DeleteFinishedTorrent', fallback=True):
+                print('Deleting torrent:', folder_name)
+                premiumize_api.delete_torrent_by_hash(download_hash)
+            finished_hash_list.add(download_hash)
+            config.read(CONFIG_PATH)
+            new_download_hash_list = config['DOWNLOADS']['Hashes'].split(',')
+            remaining_hash_list = set(new_download_hash_list) - finished_hash_list
+            config['DOWNLOADS']['Hashes'] = ','.join(remaining_hash_list)
+            save_config_file()
+        time.sleep(30)
+
+def save_config_file():
+    with open(CONFIG_PATH, 'w') as configfile:
+        config.write(configfile)
+
+upload = threading.Thread(target=upload_torrent_from_folder)
+upload.start()
+
+download = threading.Thread(target=download_finished_torrents)
+download.start()
